63072e1ef2711ac1a92e084f9f2891ad2d0a5a50
Bryan Hughes <bryanl.hughes@gmail.com>
2017-06-03 00:02:20 -0600

    Update SL with more state

--- StrategyLearner.py
+++ StrategyLearner.py
@@ -22,12 +22,15 @@ class TradingEnvironment(object):
         self.cash = sv
         self.sv = sv
         self.shares = 0
+        self.pp = 0 #Purchase Price
 
         dates = pd.date_range(sd-dt.timedelta(100), ed)
         df = util.get_data([symbol],dates)[symbol]
         normalized = df/df.ix[0]
         sma = normalized.rolling(50).mean()
-        df = pd.DataFrame(df).assign(normalized =normalized).assign(sma = sma).assign(psma = normalized / sma)[sd:]
+        std = normalized.rolling(50).std()
+        bb = bb =(normalized - (sma- std*2)) / ((sma + std*2) - (sma- std*2)) 
+        df = pd.DataFrame(df).assign(normalized =normalized).assign(sma = sma).assign(psma = normalized / sma).assign(bb=bb)[sd:]
         daily_returns = df[symbol].copy()
         daily_returns[1:] =  (df[symbol].ix[1:] / df[symbol].ix[:-1].values) -1
         daily_returns.ix[0] = 0
@@ -42,10 +45,12 @@ class TradingEnvironment(object):
     def buy(self):
         if self.shares >= 200:
           return -10000 #Invalid purchase
-        if self.verbose: print "Buy 100 @ %0.2f" %(self.current_stats[1][self.symbol])
+        closing_price = self.current_stats[1][self.symbol]
+        self.pp =pd.cut(self.df['normalized'], 10, labels=False)[self.current_stats[0]]
+        if self.verbose: print "Buy 100 @ %0.2f" %(closing_price)
         self.shares = self.shares + 100
-        self.cash = self.cash - (100 * self.current_stats[1][self.symbol] ) # Closing price
-        dr = 100 * self.current_stats[1]['dr']
+        self.cash = self.cash - (100 * closing_price ) # Closing price
+        dr = self.shares * self.current_stats[1]['dr']
         return dr #Reward
 
     def sell(self):
@@ -54,6 +59,7 @@ class TradingEnvironment(object):
         if self.verbose: print "SELL 100 @ %0.2f" %(self.current_stats[1][self.symbol])
         self.shares = self.shares - 100
         self.cash = self.cash + (100 * self.current_stats[1][self.symbol]) # Closing price
+        self.pp = 0 #Reset Purchase Price
         return (self.cash - self.sv) + (self.shares * self.current_stats[1][self.symbol]) #Reward
 
     def wait(self):
@@ -62,13 +68,17 @@ class TradingEnvironment(object):
         return dr
 
     def discritize_state(self):
+        
         norm = pd.cut(self.df['normalized'], 10, labels=False)
         psma = pd.cut(self.df['psma'], 10, labels=False)
+        bb =   pd.cut(self.df['bb'],10, labels=False)
         date = self.current_stats[0]
-        return (self.shares) + norm[date] * 10 + psma[date]
+        
+        return (bb * 1000) + (self.pp * 100) + (norm[date] * 10) + psma[date]
 
     def increment(self,action):
         # Calculate reward based on action
+        print action
         r = {
           self.action.BUY:  self.buy,
           self.action.SELL: self.sell,
@@ -96,13 +106,13 @@ class StrategyLearner(object):
   def __init__(self, verbose = False):
     self.verbose = verbose
     self.Q = QLearner.QLearner(
-        num_states = 1000,
+        num_states = 10000,
         num_actions = 3,
         alpha = 0.2,
         gamma = 0.9,
         rar = 0.5,
         radr = 0.99,
-        dyna = 200)
+        dyna = 50)
 
 
   def addEvidence(self,
